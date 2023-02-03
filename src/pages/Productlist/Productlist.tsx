d5aafbce8e0f5a605369b089710e165654636e7f
HoangAnhTuan <75159271+tuan2099@users.noreply.github.com>
2023-02-03 16:53:23 +0700

    upcode

--- src/pages/Productlist/Productlist.tsx
+++ src/pages/Productlist/Productlist.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ProductList() {
-  return <div>ProductList</div>
+  return (
+    <div>
+      <Link to='/login'> come to home page </Link>
+    </div>
+  )
 }
 
 export default ProductList
