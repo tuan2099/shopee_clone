15efe970629239b71709580e4ef951466295af4c
HoangAnhTuan <75159271+tuan2099@users.noreply.github.com>
2023-03-17 15:23:37 +0700

    up code:  "slider ảnh sản phẩm"

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,36 +1,69 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
 import InputNumber from 'src/components/InputNumber'
 import ProductRating from 'src/components/productRating'
 import { formartCurrency, formatNumberToSocialStyle, rateSale } from 'src/uitils/uitils'
 import DOMPurify from 'dompurify'
+import { Product } from 'src/type/product.type'
 
 function ProductDetail() {
   const { id } = useParams()
+  const [currentIndexImage, setCurrentIndexImage] = useState([0, 5])
+  const [hoverActiveImage, setHoverAcctiveImage] = useState('')
   // call api product detail
   const { data: productDetailData } = useQuery({
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
   const product = productDetailData?.data.data
+  const curentImages = useMemo(
+    () => (product ? product.images.slice(...currentIndexImage) : []),
+    [product, currentIndexImage]
+  ) // slide product
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      // có data và ko under file
+      setHoverAcctiveImage(product.images[0])
+    }
+  }, [product])
+
+  const chosseActive = (img: string) => {
+    // hover
+    setHoverAcctiveImage(img)
+  }
+  const buttonNextSlider = () => {
+    if (currentIndexImage[1] < (product as Product).images.length) {
+      setCurrentIndexImage((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+  const buttonPrevSlider = () => {
+    if (currentIndexImage[0] > 0) {
+      setCurrentIndexImage((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
   if (!product) return null
+
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
-                  src={product.image}
+                  src={hoverActiveImage}
                   alt={product.name}
                   className='absolute top-0 left-0 h-full bg-white object-cover'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={buttonPrevSlider}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -42,12 +75,12 @@ function ProductDetail() {
                     <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
                   </svg>
                 </button>
-                {product.images.slice(0, 5).map((img, index) => {
-                  const isActive = index === 0
+                {curentImages.map((img, index) => {
+                  const isActive = img === hoverActiveImage
                   return (
-                    <div key={img} className='relative w-full pt-[100%]'>
+                    <div key={img} className='relative w-full pt-[100%]' onMouseEnter={() => chosseActive(img)}>
                       <img
-                        src={product.image}
+                        src={img}
                         alt={product.name}
                         className='absolute top-0 left-0 h-full w-full cursor-pointer bg-white object-cover'
                       />
@@ -55,7 +88,10 @@ function ProductDetail() {
                     </div>
                   )
                 })}
-                <button className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={buttonNextSlider}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
